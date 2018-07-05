7c0b0e77cb7161c6e4d78649da24646f7f58b70b
NStephenson <nickstephenson90@gmail.com>
2018-07-04 22:59:25 -0400

    added basic validations

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,13 @@
+require 'uri';
+
 class Resource < ApplicationRecord
   has_many :lesson_resources
   has_many :lessons, through: :lesson_resources
   belongs_to :type
+
+  validates :name, :link, presence: true
+  validates :link, uniqueness: {case_sensitive: false}
+  # validates :link, format: /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/
+
+
 end
