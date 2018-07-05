b876de7720d1db6a63503f797229d457373398c5
NStephenson <nickstephenson90@gmail.com>
2018-07-04 22:56:37 -0400

    added update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :resources, only: [:show, :create]
+  resources :resources, only: [:show, :create, :update, :destroy]
+
+ get 'lesson/:slug/resources', to: 'resources#index', as: 'lesson_resources'
 end
