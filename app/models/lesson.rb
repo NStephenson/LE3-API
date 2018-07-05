96a5cb1b068a18c5c537c8545842b76d16f2c414
NStephenson <nickstephenson90@gmail.com>
2018-07-04 22:59:01 -0400

    added deslug method

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,10 @@
 class Lesson < ApplicationRecord
   has_many :lesson_resources
   has_many :resources, through: :lesson_resources
+
+
+  def deslug(slug)
+    slug.split("_").join(" ")
+  end
+
 end
